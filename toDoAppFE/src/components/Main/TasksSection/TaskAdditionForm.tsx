8899aea025b0e5f4932983671da4e04c5b297de5
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-12-04 15:27:58 +0100

    created TaskAdditionForm component

---
+++ toDoAppFE/src/components/Main/TasksSection/TaskAdditionForm.tsx
@@ -0,0 +1,9 @@
+export const TaskAdditionForm = () => {
+    return (
+        <div className={"blur-container"}>
+            <form className={"task-addition-form"}>
+                
+            </form>
+        </div>
+    )
+}
--- toDoAppFE/src/components/common/SectionLabel.tsx
+++ toDoAppFE/src/components/common/SectionLabel.tsx
@@ -1,12 +1,13 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
 interface Props {
     children: ReactNode,
-    style?: {},
+    style?: object,
 }
-export const SectionLabel = ({ children, style }: Props) => {
+
+export const SectionLabel = ({children, style}: Props) => {
     return (
-        <h5 style={{ fontFamily: "Ubuntu Mono", color: "#303030", padding: "0.3rem 1rem", ...style }}>{children}</h5>
+        <h5 style={{fontFamily: "Ubuntu Mono", color: "#303030", padding: "0.3rem 1rem", ...style}}>{children}</h5>
 
     );
 }
