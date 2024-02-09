5f0f47fe8751fdf7e4f9ffc462ae48068271a173
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2024-02-09 12:54:30 +0100

    added function that handles adding bookmarks

--- toDoAppFE/src/components/Main/BookmarksSection/AddBookmarkForm.tsx
+++ toDoAppFE/src/components/Main/BookmarksSection/AddBookmarkForm.tsx
@@ -1,19 +1,42 @@
 import {InputBox} from "../TasksSection/TaskForm/InputBox.tsx";
-import {useContext} from "react";
+import {ChangeEvent, FormEvent, useContext, useState} from "react";
 import {BookmarkContext} from "../../../contexts/bookmarkContext.tsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleXmark} from "@fortawesome/free-solid-svg-icons";
+import {reload} from "../../../utils/reloader.ts";
 
 export const AddBookmarkForm = () => {
     const {bookmarkFormActive, setBookmarkFormActive} = useContext(BookmarkContext);
+    const [bookmarkName, setBookmarkName] = useState("");
+
+    const handleBookmarkFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await fetch("http://localhost:3001/users/bookmarks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEzZDYwNTc4LWM2ZWEtNDlkNC1hNTZiLTI3YjY1OTZjYmU1NyIsImlhdCI6MTcwNjc4ODkxM30.w0hFeHXasU4TDF_F8oJ88dbzMGj69hthSceCsQCT6S4`
+            },
+            body: JSON.stringify({bookmarkName})
+        });
+        // refer user to main page after he adds the bookmark
+        reload();
+    }
 
     return (
         <div
             className={`add-bookmark-form-container ${bookmarkFormActive ? "bookmark-form-active" : "bookmark-form-disabled"}`}>
-            <form className={"add-bookmark-form"}>
+            <form onSubmit={handleBookmarkFormSubmit} className={"add-bookmark-form"}>
                 <h3>add new bookmark</h3>
-                <InputBox change={() => console.log("ok")} name={"bookmarkName"} label={"Bookmark name"} type={"text"}
-                          value={""}/>
+                <InputBox
+                    change={(e: ChangeEvent<HTMLInputElement>) => {
+                        setBookmarkName(e.target.value);
+                    }}
+                    name={"bookmarkName"}
+                    label={"Bookmark name"}
+                    type={"text"}
+                    value={bookmarkName}
+                />
             </form>
             {/*BUTTON BELOW WILL BE USED ONLY TO CLOSE THE BOOKMARK ADDITION FORM*/}
             <FontAwesomeIcon onClick={setBookmarkFormActive} icon={faCircleXmark} className={"bookmark-addition-button"}
