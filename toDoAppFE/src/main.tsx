82dba70f816ae443cb27ca4051890a2323e25ff3
Moj Komputer <zseg1ipg.mendrok.marcel@gmail.com>
2024-03-14 12:29:03 +0100

    adapting the application to PWA requirements

--- toDoAppFE/src/main.tsx
+++ toDoAppFE/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './components/App.tsx'
-import "./serviseWorker.ts";
+import "./serviseWorkerRegister.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
