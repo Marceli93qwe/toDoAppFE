e0cfce3b23736bf453edf6d48c99691b64b9ffde
Moj Komputer <zseg1ipg.mendrok.marcel@gmail.com>
2024-03-21 15:52:06 +0100

    Added "Hello" text on top of the page

--- toDoAppFE/src/components/Header/RightPanel.tsx
+++ toDoAppFE/src/components/Header/RightPanel.tsx
@@ -5,6 +5,7 @@ import {RightPanelSummary} from "./RightPanelSummary";
 export const RightPanel = () => {
     return (
         <div className={"right-panel"}>
+            <h6>hi, marcel</h6>
             <Row style={{justifyContent: "space-around", height: "9rem"}}>
                 <RightPanelItem/>
                 <RightPanelItem/>
